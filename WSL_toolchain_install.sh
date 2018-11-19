4e300fca180d53850ca3964167586e4c0a88ab5a
unknown <gvozdje@gmail.com>
2018-11-19 10:22:06 +0100

    Updated and fixed ssh configuraiton procedure and added build-essential to install list

--- WSL_toolchain_install.sh
+++ WSL_toolchain_install.sh
@@ -4,24 +4,21 @@
 sudo apt-get update
 
 #Step 1: Check for user approval
-echo "The following software will be installed: \ngradle\ngcc\ncMake\ngdb\nclang\ngit"
+echo "The following software will be installed: gradle \gcc \cMake \gdb \clang \build-essential \git"
 read -p "Do you wish to continue? Yes [Y] : No [N] " -n 1 -r
 echo "\n"
 case $REPLY in
-
-["Y"]*) #In case of positive response
+["Y"]*)                                                                     #In case of positive response
     #Step 3 Install Basic Software
-    sudo apt-get install git gradle gcc cmake clang gdb
+    sudo apt-get install gradle gcc cmake gdb clang build-essential git
 
     #Step 4 cloning and running JetBrains config script
-    mkdir ./temp
-    cd ./temp
     git clone https://github.com/JetBrains/clion-wsl.git
-    bash ubuntu_setup_env.sh
-    rmdir -r ./
+    bash clion-wsl/ubuntu_setup_env.sh
+    rm -r ./clion-wsl
     ;;
-["N"]*) #In case of negative response
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # exit shell
+["N"]*)                                                                     #In case of negative response
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1                       #Exit shell
     ;;
 *)
     echo "Invalid Input"
